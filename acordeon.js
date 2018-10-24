33b5a5902c2cd3d7a27dbcabd5f8f44140b9a69b
CynthiaGom <cynthiagom01@gmail.com>
2018-10-24 17:43:07 -0500

    v1

---
+++ acordeon.js
@@ -0,0 +1,40 @@
+
+//REGRESAR
+
+botonr = document.getElementById('r');//
+
+	botonr.addEventListener('click',reg,true);
+
+function reg(){
+	window.location.assign('calculadora.html');
+}
+
+
+//LO QUE MANTIENE GUARDADAS LAS NOTAS
+
+window.addEventListener('load',cargar, true); //cuando se carga la pag
+
+	function cargar(){
+			
+			text.innerHTML = localStorage.getItem('text'); //el texto es igual al lo que está en el localstorage
+		}
+
+
+
+
+//GUARDAR
+
+botong = document.getElementById('g');
+botong.addEventListener('click',guar, true);
+
+function guar(){
+	text=document.getElementById('text')//definimos que 'text' es lo que está dentro de txt(el documento pueh)
+	text.innerHTML=text.value //inner.html es igual al valor del texto
+	localStorage.setItem('text',text.innerHTML);//en el localstorage guardar 
+}
+
+
+
+//innerHTML (?)
+//Porque no poner .value desde el inicio? {32}
+//('text',text.innerHTML) (?)
---
+++ calculadora.js
@@ -0,0 +1,28 @@
+
+function concatenar(dato)
+{
+	dato = document.getElementById("hola").value+=dato;
+	
+}
+
+function borrar() {
+	document.getElementById('hola').value="";
+}
+
+
+function igual(){
+	eq = document.getElementById('hola').value;
+	resultado = eval(eq);
+	document.getElementById('hola').value=resultado;
+}
+
+
+function secreto(){
+	valor = document.getElementById('hola').value;
+	if (valor == "007"){
+	window.location.assign('acordeon.html');
+	}
+}
+
+
+//utiliza una variable para cada cosa
